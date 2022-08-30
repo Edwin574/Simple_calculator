4f72519d34a06abd0d3a5ebb8b26bc70712379cb
Edwin574 <owinoedwin574@gmail.com>
2022-08-30 09:36:03 +0300

    adding event listeners

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 const lightswitch = document.getElementById('light-switch')
-const body=document.getElementsByName('body')
+const body = document.querySelector('body')
+const digits = document.querySelectorAll('.digit')
+const display=document.querySelector('answer-area')
 
+console.log(digits)
 
 
 lightswitch.addEventListener('click', () => {
     lightswitch.classList.toggle('light-mode')
-    body.classList.add('light')
-
+    body.classList.toggle('light')
+    
 })
