2949cdacc29c3b4f1ad12a3c12b28e11d9af266e
Edwin574 <owinoedwin574@gmail.com>
2022-09-01 10:33:16 +0300

    added squareroot functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const lightswitch = document.getElementById("light-switch");
 const body = document.querySelector("body");
 const display = document.querySelector(".answer-area");
+const clear = document.querySelector(".clear-button");
 
 const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const squareRoot = document.querySelector('.square-root')
+const equal = document.querySelector('.equal-sign')
 
 //light switch
 
@@ -15,7 +19,32 @@ lightswitch.addEventListener("click", () => {
 
 digits.forEach((element) => {
     element.addEventListener("click", (e) => {
-        display.innerHTML = parseInt(e.target.innerText)
-        console.log(display.innerHTML);
+        let number = parseInt(e.target.innerText);
+        display.append(number);
+        // console.log(display.innerHTML);
     });
 });
+
+clear.addEventListener("click", () => {
+    display.innerHTML = "";
+});
+//appending mathematical operators
+operators.forEach((symbol) => {
+    symbol.addEventListener("click", (e) => {
+        let operator = e.target.innerText;
+
+        display.append(operator);
+    });
+});
+
+//squareroot
+
+squareRoot.addEventListener('click', () => {
+let answer=Math.sqrt(parseInt(display.innerHTML))
+    console.log(answer)
+    equal.addEventListener('click', () => {
+        display.innerHTML = ''
+        display.append(answer)
+    })
+})
+//percentage
